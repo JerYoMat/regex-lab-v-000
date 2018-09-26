40e51178211ced1e1fdb9698a5f4ea029e70e9ef
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-26 21:22:03 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[AEIOUaeiou]\w+/) != nil 
+    true 
+  else 
+    false 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/^un.+ing$/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match()
 end
 
 def valid_phone_number?(phone)
